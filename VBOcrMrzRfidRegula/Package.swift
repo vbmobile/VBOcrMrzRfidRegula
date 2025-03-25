a058e999d92e7d160edbb712b35aab151fec5b06
Tiago Silva <tiago.silva@vision-box.com>
2025-03-25 11:07:47 +0000

    PMOBILEID-4834: Add SPM

---
+++ VBOcrMrzRfidRegula/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VBOcrMrzRfidRegula",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "VBOcrMrzRfidRegula",
+            targets: ["VBOcrMrzRfidRegulaWrapper"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/regulaforensics/DocumentReaderOCRRFID-Swift-Package", from: "7.5.0"),
+        .package(url: "https://github.com/regulaforensics/DocumentReader-Swift-Package", from: "7.5.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "VBOcrMrzRfidRegula",
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/MobileIdSDKiOS/VBOcrMrzRfidRegula/VBOcrMrzRfidRegula-1.0.0.zip",
+            checksum: "597a67e3666f9e79f8f67b25864e84ac2707f4f34ebb99b5e4d8ab51875c394d"
+        ),
+        .target(
+            name: "VBOcrMrzRfidRegulaWrapper",
+            dependencies: [
+                .target(name: "VBOcrMrzRfidRegula"),
+                .product(name: "OCRRFID", package: "DocumentReaderOCRRFID-Swift-Package"),
+                .product(name: "DocumentReader", package: "DocumentReader-Swift-Package")
+            ],
+            path: "Sources",
+            sources: ["Dummy.swift"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
---
+++ VBOcrMrzRfidRegula/Sources/Dummy.swift
@@ -0,0 +1,6 @@
+/// Is used for the Package.swift in implementing projects to have a fake Swift source file.
+
+import OCRRFID
+import DocumentReader
+
+struct Dummy {}
