2b2c17c07e71c9795b88994c8e6d440028b234d0
Pedro Ferreira <pedro.ferreira@vision-box.com>
2025-04-01 21:04:41 +0100

    fix MobileIdSDKiOS version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/regulaforensics/DocumentReaderOCRRFID-Swift-Package", .upToNextMinor(from: "7.5.0")),
         .package(url: "https://github.com/regulaforensics/DocumentReader-Swift-Package", .upToNextMinor(from: "7.5.0")),
-        .package(url: "https://github.com/vbmobile/MobileIdSDKiOS", from: "8.0.0")
+        .package(url: "https://github.com/vbmobile/MobileIdSDKiOS", .upToNextMinor(from: "8.0.0"))
     ],
     targets: [
         .binaryTarget(
